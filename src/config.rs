06c9abf8d4c9020355b7acc68a8f4bb757bee89c
paina <painXiongMao@outlook.com>
2025-06-29 13:37:03 +0800

    修改了自己的ID服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["61.111.244.185"];
+pub const RS_PUB_KEY: &str = "1VXvvhnJ0O17LuWm6RfmiF7HxF2ULlllXGm46ReDYtA=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
